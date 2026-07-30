307b2a12ffb837f4700969b9968dedc562e35ea4
Mahir <mahirrasidagic19@gmail.com>
2026-07-30 21:12:36 +0200

    fix againn

--- backend/src/controllers/restoraunt.controller.ts
+++ backend/src/controllers/restoraunt.controller.ts
@@ -1,5 +1,6 @@
 import type { Response } from "express";
-import type { AuthRequest } from "../middleware/auth.middleware.js";
+import type { AuthRequest } from "../types/express.type.js";
+
 import {
   createRestaurantWithOwner,
   getAllRestaurants,
